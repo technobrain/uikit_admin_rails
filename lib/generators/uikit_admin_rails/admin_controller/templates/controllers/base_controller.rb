00e707096c0f17aa61fe5195fbb796ea65d77bba
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2018-02-25 23:51:37 +0900

    add admin base controller, views

---
+++ lib/generators/uikit_admin_rails/admin_controller/templates/controllers/base_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+<% ns = UikitAdminRails.configuration.admin_controller_namespace -%>
+module <%= ns.to_s.titleize %>
+  class BaseController < ApplicationController
+    # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+    layout '<%= ns.to_s %>'
+    # before_action :require_login
+
+    def index
+      render 'admin/index'
+    end
+
+    private
+
+    # # Sorcery
+    # def not_authenticated
+    #   redirect_to sign_in_path, notice: 'Require login.'
+    # end
+
+    # # Pundit
+    # def user_not_authorized
+    #   flash[:alert] = 'You are not authorized to perform this action.'
+    #   redirect_to(request.referer || root_path)
+    # end
+  end
+end
