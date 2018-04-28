4584ee93668ef625c7fd9184e361753ce32c4c9b
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2018-04-28 15:31:47 +0900

    separate ukit generator, install uikit task

--- lib/generators/uikit_admin_rails/install/install_generator.rb
+++ lib/generators/uikit_admin_rails/install/install_generator.rb
@@ -3,6 +3,13 @@
 module UikitAdminRails
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
+    
+    def copy_scss_theme
+      say "Appending Uikit setup code to #{Rails.root.join('app', 'assets', 'stylesheets')}/admin-theme.scss"
+      source = '../templates/admin-theme.scss'
+      dest = "#{UikitAdminRails.config.theme_entry_path || Rails.root.join('app', 'assets', 'stylesheets')}/admin-theme.scss"
+      copy_file source, dest
+    end
 
     def copy_initializer_file
       puts 'create initializers/uikit_admin.rb'
@@ -11,18 +18,11 @@ module UikitAdminRails
       copy_file source, dest
     end
 
-    def uikit_install
-      say "Appending Uikit setup code to #{UikitAdminRails.config.theme_entry_path}/admin-theme.scss"
-      source = '../templates/admin-theme.scss'
-      dest = "#{UikitAdminRails.config.theme_entry_path}/admin-theme.scss"
-      copy_file source, dest
+    # def install_uikit
+    #   say "Installing UIkit"
+    #   system "rake uikit_admin_rails:install:uikit"
 
-      # say "Installing all Stimulus dependencies"
-      # execute_command "yarn add uikit"
-      system "rake uikit_admin_rails:install:uikit"
-
-      # say "UikitAdminRails now supports UIkit 🎉", :green
-    end
-
+    #   say "UikitAdminRails now supports UIkit 🎉", :green
+    # end
   end
 end
