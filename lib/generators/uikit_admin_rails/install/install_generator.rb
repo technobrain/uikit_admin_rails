790a85d53a08f838e4c9146841cc22fa8c1e4ce4
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2018-10-11 13:40:36 +0900

    fix test

--- lib/generators/uikit_admin_rails/install/install_generator.rb
+++ lib/generators/uikit_admin_rails/install/install_generator.rb
@@ -12,7 +12,7 @@ module UikitAdminRails
     end
 
     def copy_initializer_file
-      # puts 'create initializers/uikit_admin.rb'
+      say puts 'create initializers/uikit_admin.rb'
       source = '../templates/uikit_admin.rb'
       dest = Rails.root.join('config', 'initializers', 'uikit_admin.rb')
       copy_file source, dest
